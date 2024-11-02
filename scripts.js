73d2bfdae8b823a05a70691bccea0ada0a41c3a4
codeylee05 <mlefa5002@gmail.com>
2024-11-02 14:49:45 +0200

    Secondary campus navbar and responsive

--- scripts.js
+++ scripts.js
@@ -4,3 +4,17 @@ const mobileMenu = document.getElementById('mobile-menu');
         mobileMenu.addEventListener('click', () => {
             navLinks.classList.toggle('active');
         });
+
+
+/* Secondary navbar */
+document.querySelector('.campuses-link > a').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent default link behavior
+    this.parentElement.classList.toggle('active'); // Toggle 'active' class on parent <li>
+});
+
+let lastScrollTop = 0;
+window.addEventListener('scroll', function () {
+    const campusesLink = document.querySelector('.campuses-link');
+    campusesLink.classList.remove('active'); // Hide the submenu on scroll
+});
+
